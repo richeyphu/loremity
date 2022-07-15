2f7af2e70c9a0dc8dc544d03b9f0204b7091e245
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-07-15 17:43:17 +0700

    Format with Prettier

--- svelte.config.js
+++ svelte.config.js
@@ -3,22 +3,22 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: [
+        preprocess({
+            postcss: true,
+        }),
+    ],
 
-	kit: {
-		adapter: adapter(),
+    kit: {
+        adapter: adapter(),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
+        // Override http methods in the Todo forms
+        methodOverride: {
+            allowed: ['PATCH', 'DELETE'],
+        },
+    },
 };
 
 export default config;
