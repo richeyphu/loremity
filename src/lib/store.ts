d70bf2f002b41e66ae2a5f3ff40100ea5aa3910f
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-07-17 06:00:26 +0700

    Add theme detection

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,3 +1,14 @@
 import { writable } from 'svelte/store';
+import { browser } from '$app/env';
 
-export const darkTheme = writable(false);
+let themeValue = false;
+if (browser) {
+    if (
+        localStorage.theme === 'dark' ||
+        (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+        themeValue = true;
+    }
+}
+
+export const darkTheme = writable(themeValue);
