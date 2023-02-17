90523be4131f40793d0d0764bff3c448f7053d42
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-02-17 20:20:33 +0700

    Update infinite scroll test

--- tests/test.ts
+++ tests/test.ts
@@ -12,16 +12,11 @@ test('home page has expected h1', async ({ page }) => {
 
 test('home page has expected footer to be unreachable', async ({ page }) => {
 	await page.goto('/');
-	await page.setViewportSize({ width: 1920, height: 1080 });
 
-	for (let i = 0; i < 100; i++) {
-		await page.mouse.wheel(0, 1000);
-		await page.waitForTimeout(100);
-	}
-
-	const isVisible = await page.evaluate((selector) => {
+	// Check if element is visible in viewport function
+	const checkViewport = (selector: Parameters<typeof page.evaluate>[0]) => {
 		let isVisible = false;
-		const element = document.querySelector(selector);
+		const element = document.querySelector(selector as keyof HTMLElementTagNameMap);
 		if (element) {
 			const rect = element.getBoundingClientRect();
 			if (rect.top >= 0 && rect.left >= 0) {
@@ -33,9 +28,40 @@ test('home page has expected footer to be unreachable', async ({ page }) => {
 			}
 		}
 		return isVisible;
-	}, 'footer');
+	};
 
-	expect(isVisible).toBeFalsy();
+	// Set viewport size for desktop
+	await page.setViewportSize({ width: 1536, height: 864 })
+
+	// Try to scroll to bottom
+	for (let i = 0; i < 100; i++) {
+		await page.mouse.wheel(0, 1000);
+		await page.waitForTimeout(10);
+	}
+
+	// Check if footer is visible in desktop viewport
+	const isVisibleInDesktop = await page.evaluate(checkViewport, 'footer');
+
+	// Expect footer to be unreachable
+	expect(isVisibleInDesktop).toBeFalsy();
+
+	// Reload page
+	await page.reload();
+
+	// Set viewport size for mobile
+	await page.setViewportSize({ width: 393, height: 852 });
+
+	// Try to scroll to bottom
+	for (let i = 0; i < 100; i++) {
+		await page.mouse.wheel(0, 1000);
+		await page.waitForTimeout(10);
+	}
+
+	// Check if footer is visible in mobile viewport
+	const isVisibleInMobile = await page.evaluate(checkViewport, 'footer');
+
+	// Expect footer to be unreachable
+	expect(isVisibleInMobile).toBeFalsy();
 });
 
 // test('home page can be accessed while offline', async ({ page, context }) => {
