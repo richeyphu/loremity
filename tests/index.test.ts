395d83fe6ae9d13d1f560a5123fa232a591acf22
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-02-18 04:54:04 +0700

    Add group test declaration

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,13 +1,15 @@
 import { expect, test } from '@playwright/test';
 
-test('home page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Loremity' })).toBeVisible();
-});
+test.describe('Page Heading', () => {
+	test('home page has expected h1', async ({ page }) => {
+		await page.goto('/');
+		await expect(page.getByRole('heading', { name: 'Loremity' })).toBeVisible();
+	});
 
-test('about page has expected h1', async ({ page }) => {
-	await page.goto('/about');
-	await expect(page.getByRole('heading', { name: 'About this app' })).toBeVisible();
+	test('about page has expected h1', async ({ page }) => {
+		await page.goto('/about');
+		await expect(page.getByRole('heading', { name: 'About this app' })).toBeVisible();
+	});
 });
 
 // test('home page can be accessed while offline', async ({ page, context }) => {
--- tests/loremity.test.ts
+++ tests/loremity.test.ts
@@ -1,59 +1,61 @@
 import { expect, test } from '@playwright/test';
 import type { Page } from '@playwright/test';
 
-// Check if element is visible in viewport function
-const checkViewport = (selector: Parameters<Page['evaluate']>[0]) => {
-	let isVisible = false;
-	const element = document.querySelector(selector as keyof HTMLElementTagNameMap);
-	if (element) {
-		const rect = element.getBoundingClientRect();
-		if (rect.top >= 0 && rect.left >= 0) {
-			const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-			const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-			if (rect.right <= vw && rect.bottom <= vh) {
-				isVisible = true;
+test.describe('Infinite Scroll', () => {
+	// Check if element is visible in viewport function
+	const checkViewport = (selector: Parameters<Page['evaluate']>[0]) => {
+		let isVisible = false;
+		const element = document.querySelector(selector as keyof HTMLElementTagNameMap);
+		if (element) {
+			const rect = element.getBoundingClientRect();
+			if (rect.top >= 0 && rect.left >= 0) {
+				const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+				const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+				if (rect.right <= vw && rect.bottom <= vh) {
+					isVisible = true;
+				}
 			}
 		}
-	}
-	return isVisible;
-};
+		return isVisible;
+	};
 
-// Page scroll down function
-const scrollDown = async (page: Page, n: number) => {
-	for (let i = 0; i < n; i++) {
-		await page.mouse.wheel(0, 1000);
-		await page.waitForTimeout(10);
-	}
-};
+	// Page scroll down function
+	const scrollDown = async (page: Page, n: number) => {
+		for (let i = 0; i < n; i++) {
+			await page.mouse.wheel(0, 1000);
+			await page.waitForTimeout(10);
+		}
+	};
+	
+	test.beforeEach(async ({ page }) => {
+		await page.goto('/');
+	});
 
-test('home page has expected footer to be unreachable (desktop)', async ({ page }) => {
-	await page.goto('/');
+	test('expected footer to be unreachable (desktop)', async ({ page }) => {
+		// Set viewport size for desktop
+		await page.setViewportSize({ width: 1536, height: 864 });
 
-	// Set viewport size for desktop
-	await page.setViewportSize({ width: 1536, height: 864 });
+		// Try to scroll to bottom
+		await scrollDown(page, 100);
 
-	// Try to scroll to bottom
-	await scrollDown(page, 100);
+		// Check if footer is visible in desktop viewport
+		const isVisible = await page.evaluate(checkViewport, 'footer');
 
-	// Check if footer is visible in desktop viewport
-	const isVisible = await page.evaluate(checkViewport, 'footer');
+		// Expect footer to be unreachable
+		expect(isVisible).toBeFalsy();
+	});
 
-	// Expect footer to be unreachable
-	expect(isVisible).toBeFalsy();
-});
+	test('expected footer to be unreachable (mobile)', async ({ page }) => {
+		// Set viewport size for mobile
+		await page.setViewportSize({ width: 393, height: 852 });
 
-test('home page has expected footer to be unreachable (mobile)', async ({ page }) => {
-	await page.goto('/');
+		// Try to scroll to bottom
+		await scrollDown(page, 100);
 
-	// Set viewport size for mobile
-	await page.setViewportSize({ width: 393, height: 852 });
+		// Check if footer is visible in mobile viewport
+		const isVisible = await page.evaluate(checkViewport, 'footer');
 
-	// Try to scroll to bottom
-	await scrollDown(page, 100);
-
-	// Check if footer is visible in mobile viewport
-	const isVisible = await page.evaluate(checkViewport, 'footer');
-
-	// Expect footer to be unreachable
-	expect(isVisible).toBeFalsy();
+		// Expect footer to be unreachable
+		expect(isVisible).toBeFalsy();
+	});
 });
